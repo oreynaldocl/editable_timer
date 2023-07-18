8e643687010c26d0086c62a48fb18d9e156824a6
Oscar <oscar.callisaya@niceincontact.com>
2023-07-18 11:54:19 -0400

    Created basic project (#1)

---
+++ EditableTimer/EditableTimer/ITimerExecuter.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using System;
+
+namespace EditableTimer
+{
+    /// <summary>
+    /// Define the implementations for executing and updating timer.
+    /// Every new update of timer needs to create a new Thread.
+    /// It is expected that only one thread at time is related with TimerExecuter
+    /// </summary>
+    public interface ITimerExecuter
+    {
+        /// <summary>
+        /// Unique identifer that will be used for TimerManager
+        /// </summary>
+        int Identifier { get; }
+        /// <summary>
+        /// Used for calculate next execution if there is any error
+        /// </summary>
+        TimeSpan DefaultPeriod { get; }
+        /// <summary>
+        /// After the current wait time, this method is executed
+        /// </summary>
+        /// <returns></returns>
+        Task ExecuteHanlder();
+        // TODO Review if we need to call CalculateNextTime after FailureHandler
+        /// <summary>
+        /// If there is any failure in process this is executed
+        /// </summary>
+        /// <returns></returns>
+        Task FailureHandler();
+        // TODO  Review if we need to use TimeSpan / DateTime
+        /// <summary>
+        /// Returns when will be next execution. Called after ExecuteTask or FailureMethod
+        /// </summary>
+        /// <returns></returns>
+        Task<TimeSpan> CalculateNextTime();
+    }
+}
---
+++ EditableTimer/EditableTimer/ITimerManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EditableTimer
+{
+    public interface ITimerManager
+    {
+        /// <summary>
+        /// Register an executer, next time execution will be after dueInitialTime.
+        /// </summary>
+        /// <param name="executer"></param>
+        /// <param name="dueInitialTime"></param>
+        void RegisterTimer(ITimerExecuter executer, TimeSpan dueInitialTime);
+        /// <summary>
+        /// Removes from manager the executer, it is expected to be call while dispose or when timer is not required anymore
+        /// </summary>
+        /// <param name="executer"></param>
+        void UnregisterTimer(ITimerExecuter executer);
+        /// <summary>
+        /// Cancel the current wait time and create a wait based on newTime param
+        /// </summary>
+        /// <param name="executer"></param>
+        /// <param name="newTime"></param>
+        void ChangeWaitTime(ITimerExecuter executer, TimeSpan newTime);
+    }
+}
