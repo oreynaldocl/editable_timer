936ba9407986ea5986522d6ec76aeb76d6b9692e
Oscar <oscar.callisaya@niceincontact.com>
2023-07-19 14:45:42 -0400

    Updated an example of failover.

--- EditableTimer/ConsoleClient/Program.cs
+++ EditableTimer/ConsoleClient/Program.cs
@@ -1,35 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using EditableTimer;
+using System;
 using System.Threading;
-using System.Threading.Tasks;
-using EditableTimer;
 
 namespace ConsoleClient
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             Logger log = new Logger();
             TimerManager manager = new TimerManager(log);
 
             Console.WriteLine($"{DateTime.UtcNow.ToString("HH:mm:ss.ffff")} T#{Thread.CurrentThread.ManagedThreadId} Creating an executer");
-            // CREATION EXECUTER
+
             SimpleExecuter simple1 = new SimpleExecuter(1, manager);
-            List<SimpleExecuter> list = new List<SimpleExecuter>();
-            for (int i = 0; i < 15; i++) {
-                list.Add(new SimpleExecuter(i + 2, manager));
-            }
 
-            Thread.Sleep(1000);
-            Console.WriteLine($"{DateTime.UtcNow.ToString("HH:mm:ss.ffff")} T#{Thread.CurrentThread.ManagedThreadId} Updating to wait 2 secs");
-            manager.ChangeWaitTime(simple1, TimeSpan.FromSeconds(2));
-
-            await Task.Delay(35000);
-            Console.WriteLine($"{DateTime.UtcNow.ToString("HH:mm:ss.ffff")} T#{Thread.CurrentThread.ManagedThreadId} Unregister an executor");
-            manager.UnregisterTimer(simple1);
-
-            Console.ReadKey();
+            Console.ReadLine();
         }
     }
 }
--- EditableTimer/ConsoleClient/SimpleExecuter.cs
+++ EditableTimer/ConsoleClient/SimpleExecuter.cs
@@ -5,6 +5,11 @@ using System.Threading.Tasks;
 
 namespace ConsoleClient
 {
+    /// <summary>
+    /// Example class that just logs message
+    /// And simulate errors.
+    /// Class will try run an incremental failover
+    /// </summary>
     public class SimpleExecuter : ITimerExecuter
     {
         private readonly ITimerManager _manager;
@@ -13,7 +18,11 @@ namespace ConsoleClient
         private int identifier;
         public int Identifier => identifier;
 
-        public TimeSpan DefaultPeriod => TimeSpan.FromSeconds(5);
+        private int executionCounter = 0;
+        private int errroFrom = 5;
+
+        private int incrementalAttempt = 0;
+        private int[] incrementalTime = new int[] { 100, 200, 4000, 800, 1600, 3200, 6400, 12800, 25600, 51200, 102400 };
 
         public SimpleExecuter(int identifier, ITimerManager manager)
         {
@@ -21,23 +30,44 @@ namespace ConsoleClient
             _manager = manager;
             _random = new Random();
 
-            _manager.RegisterTimer(this, TimeSpan.FromSeconds(10));
+            _manager.RegisterTimer(this, TimeSpan.FromSeconds(2));
         }
 
         public Task<TimeSpan> CalculateNextTime()
         {
-            return Task.FromResult(TimeSpan.FromSeconds(_random.Next(5, 15)));
+            return Task.FromResult(TimeSpan.FromSeconds(_random.Next(2, 5)));
         }
 
         public async Task ExecuteHandler()
         {
+            if (executionCounter >= errroFrom)
+                throw new Exception("Forcing an error for start a failover process");
+            Console.WriteLine($"{DateTime.UtcNow.ToString("HH:mm:ss.ffff")} [{identifier}-T#{Thread.CurrentThread.ManagedThreadId}] EXECUTING #{executionCounter} Just execute a simple log ###");
+            executionCounter++;
             await Task.Delay(500);
-            Console.WriteLine($"{DateTime.UtcNow.ToString("HH:mm:ss.ffff")} [{identifier}-T#{Thread.CurrentThread.ManagedThreadId}] EXECUTING Just execute a simple log ###");
         }
 
         public Task FailureHandler()
         {
-            throw new NotImplementedException();
+            TimeSpan timeSpan = BuildIncrementalPeriod(incrementalAttempt);
+            incrementalAttempt++;
+
+            _manager.ChangeWaitTime(this, timeSpan);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _manager.UnregisterTimer(this);
+        }
+
+        internal TimeSpan BuildIncrementalPeriod(int retryCounter)
+        {
+            int periodMs = 120000;
+            if (retryCounter < incrementalTime.Length)
+                periodMs = incrementalTime[retryCounter];
+
+            return TimeSpan.FromMilliseconds(periodMs);
         }
     }
 }
